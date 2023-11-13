47591e1b7d2a50fbc30837f14768deec79b6195f
honeybadger05 <144347919+honeybadger05@users.noreply.github.com>
2023-11-13 18:35:04 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -29,6 +29,21 @@ int _printf(const char *format, ...)
 		}
 		start = p;
 		p++;
-	va_end(ap);
+		while (get_flag(p, &params)) /* while char at p is flag character */
+		{
+			p++; /* next character */
+		}
+		p = get_width(p, &params, list);
+		p = get_precision(p, &params, list);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+					params.l_modifier || params.h_modifier ? p - 1 : 0);
+		else
+			sum += get_print_func(p, list, &params);
+	}
+	_putchar(BUF_FLUSH);
+	va_end(list);
 	return (sum);
 }
