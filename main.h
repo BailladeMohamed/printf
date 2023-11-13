6abc50779a17b6a8bccec8352f05788e43f6426d
honeybadger05 <144347919+honeybadger05@users.noreply.github.com>
2023-11-13 22:24:15 +0100

    Update main.h

--- main.h
+++ main.h
@@ -6,10 +6,13 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
-int print_char(va_list c)
-int print_porcentage(void)
-int selector(const char *format, va_list args, int printed)
-int print_string(va_list s)
+int print_char(va_list c);
+int print_porcentage(void);
+int selector(const char *format, va_list args, int printed);
+int print_string(va_list s);
 int _putchar(char c);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+
 
 #endif
