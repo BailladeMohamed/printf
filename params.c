ac069d9cf8684648fdcf512e19b95ed4876b3ce0
honeybadger05 <144347919+honeybadger05@users.noreply.github.com>
2023-11-13 19:03:37 +0100

    Update params.c

--- params.c
+++ params.c
@@ -7,18 +7,18 @@
  *
  * Return: bytes printed
  */
-int print_hex(va_list ap, params_t *params)
+int print_hex(va_list list, params_t *params)
 {
 	unsigned long l;
 	int c = 0;
 	char *str;
 
 	if (params->l_modifier)
-		l = (unsigned long)va_arg(ap, unsigned long);
+		l = (unsigned long)va_arg(list, unsigned long);
 	else if (params->h_modifier)
-		l = (unsigned short int)va_arg(ap, unsigned int);
+		l = (unsigned short int)va_arg(list, unsigned int);
 	else
-		l = (unsigned int)va_arg(ap, unsigned int);
+		l = (unsigned int)va_arg(list, unsigned int);
 	str = convert(l, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
 	if (params->hashtag_flag && l)
 	{
@@ -36,18 +36,18 @@ int print_hex(va_list ap, params_t *params)
  *
  * Return: bytes printed
  */
-int print_HEX(va_list ap, params_t *params)
+int print_HEX(va_list list, params_t *params)
 {
 	unsigned long l;
 	int c = 0;
 	char *str;
 
 	if (params->l_modifier)
-		l = (unsigned long)va_arg(ap, unsigned long);
+		l = (unsigned long)va_arg(list, unsigned long);
 	else if (params->h_modifier)
-		l = (unsigned short int)va_arg(ap, unsigned int);
+		l = (unsigned short int)va_arg(list, unsigned int);
 	else
-		l = (unsigned int)va_arg(ap, unsigned int);
+		l = (unsigned int)va_arg(list, unsigned int);
 	str = convert(l, 16, CONVERT_UNSIGNED, params);
 	if (params->hashtag_flag && l)
 	{
@@ -65,9 +65,9 @@ int print_HEX(va_list ap, params_t *params)
  *
  * Return: bytes printed
  */
-int print_binary(va_list ap, params_t *params)
+int print_binary(va_list list, params_t *params)
 {
-	unsigned int n = va_arg(ap, unsigned int);
+	unsigned int n = va_arg(list, unsigned int);
 	char *str = convert(n, 2, CONVERT_UNSIGNED, params);
 	int c = 0;
 
@@ -84,18 +84,18 @@ int print_binary(va_list ap, params_t *params)
  *
  * Return: bytes printed
  */
-int print_octal(va_list ap, params_t *params)
+int print_octal(va_list list, params_t *params)
 {
 	unsigned long l;
 	char *str;
 	int c = 0;
 
 	if (params->l_modifier)
-		l = (unsigned long)va_arg(ap, unsigned long);
+		l = (unsigned long)va_arg(list, unsigned long);
 	else if (params->h_modifier)
-		l = (unsigned short int)va_arg(ap, unsigned int);
+		l = (unsigned short int)va_arg(list, unsigned int);
 	else
-		l = (unsigned int)va_arg(ap, unsigned int);
+		l = (unsigned int)va_arg(list, unsigned int);
 	str = convert(l, 8, CONVERT_UNSIGNED, params);
 
 	if (params->hashtag_flag && l)
