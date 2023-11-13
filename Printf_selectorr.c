2f566c7761447c184fc5a6dd2e7e724746101dab
BailladeMohamed <baillademohamed@gmail.com>
2023-11-13 14:12:00 -0800

    compiling

--- Printf_selectorr.c
+++ Printf_selectorr.c
@@ -5,6 +5,7 @@
  * @format: The format specifier
  * @args: List of arguments
  * @p_char: Number of printed characters
+ *
  * Return: The updated count of printed characters
  */
 int selector(const char *format, va_list args, int p_char)
@@ -13,19 +14,22 @@ int selector(const char *format, va_list args, int p_char)
 	{
 		case 'd':
 		case 'i':
-			p_char = printf_int(args, p_char);
+			p_char = printf_int(args);
 			break;
 		case 'c':
 			_putchar(va_arg(args, int));
 			p_char++;
 			break;
 		case 's':
-			p_char = print_string(args, p_char);
+			p_char = print_string(args);
 			break;
 		case '%':
 			_putchar('%');
 			p_char++;
 			break;
+		default:
+			break;
 	}
+
 	return (p_char);
 }
