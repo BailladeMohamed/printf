b76566d7d1d0872dcd44492c4ab74dde6abfdb6a
honeybadger05 <144347919+honeybadger05@users.noreply.github.com>
2023-11-13 22:21:29 +0100

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _printf - our printf function
+ * @format: The format string
+ *
+ * Return: The number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+va_list args;
+int p_char = 0;
+char ch;
+
+if (format == NULL)
+return (-1);
+
+va_start(args, format);
+
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+if (*format == '\0')
+{
+va_end(args);
+return (-1);
+}
+/* Handle format specifiers */
+format++;
+}
+else
+{
+ch = *format;
+write(1, &ch, 1);
+p_char++;
+format++;
+}
+}
+
+va_end(args);
+
+return (p_char);
+}
