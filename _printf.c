3c52ae0172e0108f6856306355dbe30452e51165
honeybadger05 <144347919+honeybadger05@users.noreply.github.com>
2023-11-13 18:28:35 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,50 +1,34 @@
 #include "main.h"
 
 /**
-*_printf - printfs anything
-* @format: the format string
-*
-* Return: number of bytes printed
-*/
+ * _printf - prints and input into the standard output
+ * @format: the format string
+ * Return: number of bytes printed
+ */
 
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
+int _printf(const char *format, ...)
+{
+	int sum = 0;
+	va_list list;
+	char *p, *start;
 
-    int count = 0; 
+	params_t params = PARAMS_INIT;
 
-    while (*format) {
-        if (*format == '%') {
-            format++;
-            
-            switch (*format) {
-                case 'c':
-                    count += putchar(va_arg(args, int));
-                    break;
-                case 's':
-                    count += printf("%s", va_arg(args, char *));
-                    break;
-                case '%':
-                    count += putchar('%');
-                    break;
-                default:
-                   
-                    count += putchar('%');
-                    count += putchar(*format);
-                    break;
-            }
-        } else {
-            count += putchar(*format);
-        }
-
-        format++;
-    }
-
-    va_end(args);
-    return count;
-}
-
-int main() {
-    _printf("Hello, %s! The answer is %d%%. Character: %c\n", "world", 42, 'A');
-    return 0;
+	va_start(list, format);
+	if (!format || (format[0] == '%' && !format[1]))/* checking for NULL char */
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
+	{
+		init_params(&params, list);
+		if (*p != '%')/*checking for the % specifier*/
+		{
+			sum += _putchar(*p);
+			continue;
+		}
+		start = p;
+		p++;
+	va_end(ap);
+	return (sum);
 }
